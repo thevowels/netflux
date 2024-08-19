5d5462951dc29314f0203227073b6f254c162822
thevowels <aungphyohtet1@gmail.com>
2024-08-20 02:20:58 +0630

    Configs for cloudflare pages

--- app/abc/page.tsx
+++
@@ -1,8 +0,0 @@
-const Page = () =>{
-    return(
-        <>
-            <h1>ABC Page</h1>
-        </>
-    )
-}
-export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
         <h1>
-            Netflix clone
+            Netflix clone updated
         </h1>
     </>
   );
